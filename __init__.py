3046c4a6208be95e88560ffdeadeb7757fef2863
luluperet <luluisco@yahoo.fr>
2016-02-17 02:31:30 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-import soccersimulator, soccersimulator.settings,math,AllStrategies as AllS, As
+import soccersimulator, soccersimulator.settings,math,Walter as W
 from soccersimulator.settings import *
 from soccersimulator import  SoccerTeam as STe,SoccerMatch as SM, Player, SoccerTournament as ST, BaseStrategy as AS, SoccerAction as SA, Vector2D as V2D,SoccerState as SS
 settings=soccersimulator.settings
 
 
 
-team1= STe("psg",[Player("L1a1ss",AllS.all(2))])
-team2= STe("psg",[Player("L1a1ss",AllS.all(2)),Player("Ibra",AllS.all(3))])
-team4=STe("mars",Player("L1a1ss",AllS.all(2)),Player("Ibra",AllS.all(1)),Player("Ibra",AllS.all(3)),Player("L1a1s",AllS.all(2))])
+team1= STe("psg",[Player("Thiago Silva",W.all(2))])
+team2= STe("psg",[Player("Thiago Silva",W.all(2)),Player("Ibra",W.all(3))])
+team4=STe("mars",[Player("Thiago Silva",W.all(2)),Player("Ibra",W.all(1)),Player("Cavani",W.all(3)),Player("Thiago Silva",W.all(2))])
